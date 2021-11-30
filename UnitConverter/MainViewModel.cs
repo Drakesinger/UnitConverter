11a9e9a0983e5e8ac42e4a1b9ae5e18d281a7b99
Horia Mut <horiamut@msn.com>
2021-11-30 23:41:50 +0100

    Add project files.

---
+++ UnitConverter/MainViewModel.cs
@@ -0,0 +1,225 @@
+﻿using CK.UnitsOfMeasure;
+
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace UnitConverter
+{
+    public enum QuantityIdentifier : int
+    {
+        None = 0,
+        Quantity1 = 1,
+        Quantity2 = 2,
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<MeasureUnit> AvailableUnits { get; set; }
+
+        public Quantity Quantity1
+        {
+            get => quantity1; 
+            set
+            {
+                quantity1 = value; 
+                OnPropertyChanged("Quantity1");
+                OnPropertyChanged("DisplayValue1");
+            }
+        }
+        public Quantity Quantity2
+        {
+            get => quantity2;
+            set
+            {
+                quantity2 = value;
+                OnPropertyChanged("Quantity2");
+                OnPropertyChanged("DisplayValue2");
+            }
+        }
+        public string DisplayValue1
+        {
+            get
+            {
+                if (!HasEnteredQuantity1)
+                {
+                    return DEFAULT_DISPLAY_VALUE_1_LABEL;
+                }
+                else
+                {
+                    string valueRepresentation = Quantity1.Value.ToString();
+                    return valueRepresentation;
+                }
+            }
+
+            set
+            {
+                double newValue = 0;
+                if (double.TryParse(value, out newValue))
+                {
+                    Quantity1 = newValue.WithUnit(selectedUnit1);
+                    HasEnteredQuantity1 = true;
+                    OnPropertyChanged("DisplayValue1");
+                }
+            }
+        }
+        public string DisplayValue2
+        {
+            get
+            {
+                if (!HasEnteredQuantity2)
+                {
+                    return DEFAULT_DISPLAY_VALUE_2_LABEL;
+                }
+                else
+                {
+                    string valueRepresentation = Quantity2.Value.ToString();
+                    return valueRepresentation;
+                }
+            }
+
+            set
+            {
+                double newValue = 0;
+                if (double.TryParse(value, out newValue))
+                {
+                    Quantity2 = newValue.WithUnit(selectedUnit2);
+                    HasEnteredQuantity2 = true;
+                    OnPropertyChanged("DisplayValue2");
+                }
+
+            }
+        }
+
+        private bool HasEnteredQuantity1 = false;
+        private bool HasEnteredQuantity2 = false;
+
+        private MeasureUnit selectedUnit1 = MeasureUnit.None;
+        private MeasureUnit selectedUnit2 = MeasureUnit.None;
+
+        private Quantity quantity1 = 0.WithUnit(MeasureUnit.None);
+        private Quantity quantity2 = 0.WithUnit(MeasureUnit.None);
+
+        private const string DEFAULT_DISPLAY_VALUE_1_LABEL = "Enter a number to convert here";
+        private const string DEFAULT_DISPLAY_VALUE_2_LABEL = "It's equivalent will appear here";
+
+        public MainViewModel()
+        {
+            GenerateDefaultUnits();
+        }
+
+        private void GenerateDefaultUnits()
+        {
+            AvailableUnits = new ObservableCollection<MeasureUnit>();
+
+            var refixes = MeasureStandardPrefix.All;
+            foreach (var prefix in refixes)
+            {
+                var unitWithPrefix = prefix.On(MeasureUnit.Metre);
+                AvailableUnits.Add(unitWithPrefix);
+            }
+        }
+
+        internal void OnValueChange(QuantityIdentifier userEntry)
+        {
+            switch (userEntry)
+            {
+                case QuantityIdentifier.Quantity1:
+                    HasEnteredQuantity1 = true;
+                    break;
+                case QuantityIdentifier.Quantity2:
+                    HasEnteredQuantity2 = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        internal void OnUnitSelectionChanged(MeasureUnit selectedUnit, QuantityIdentifier entry)
+        {
+            if (selectedUnit != null)
+            {
+
+                switch (entry)
+                {
+                    case QuantityIdentifier.Quantity1:
+                        selectedUnit1 = selectedUnit;
+                        PerformConversion(QuantityIdentifier.Quantity1);
+                        break;
+                    case QuantityIdentifier.Quantity2:
+                        selectedUnit2 = selectedUnit;
+                        PerformConversion(QuantityIdentifier.Quantity2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        internal void PerformConversion(QuantityIdentifier source = QuantityIdentifier.None, string text = "")
+        {
+            switch (source)
+            {
+                case QuantityIdentifier.Quantity1:
+                    {
+                        ConvertQuantity1ToSelectedUnit2(text);
+                    }
+                    break;
+                case QuantityIdentifier.Quantity2:
+                    {
+                        ConvertQuantity2ToSelectedUnit1(text);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ConvertQuantity2ToSelectedUnit1(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                double newValue = 0;
+                if (double.TryParse(text, out newValue))
+                {
+                    Quantity2 = newValue.WithUnit(selectedUnit2);
+                    HasEnteredQuantity1 = true;
+                }
+            }
+
+            if (Quantity2.CanConvertTo(selectedUnit1))
+            {
+                Quantity1 = Quantity2.ConvertTo(selectedUnit1);
+            }
+        }
+
+        private void ConvertQuantity1ToSelectedUnit2(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                double newValue = 0;
+                if (double.TryParse(text, out newValue))
+                {
+                    Quantity1 = newValue.WithUnit(selectedUnit1);
+                    HasEnteredQuantity1 = true;
+                }
+            }
+            if (Quantity1.CanConvertTo(selectedUnit2))
+            {
+                Quantity2 = Quantity1.ConvertTo(selectedUnit2);
+            }
+        }
+
+        private void OnPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+    }
+
+}
---
+++ UnitConverter/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using CK.UnitsOfMeasure;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnitConverter
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel ViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+            DataContext = ViewModel;
+        }
+
+        private void Unit1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                var selectedUnit = e.AddedItems[0] as MeasureUnit;
+                ViewModel?.OnUnitSelectionChanged(selectedUnit, QuantityIdentifier.Quantity1);
+            }
+        }
+
+        private void Unit2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                var selectedUnit = e.AddedItems[0] as MeasureUnit;
+                ViewModel?.OnUnitSelectionChanged(selectedUnit, QuantityIdentifier.Quantity2);
+            }
+        }
+
+        private void Value1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ViewModel?.OnValueChange(QuantityIdentifier.Quantity1);
+        }
+
+        private void Value2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ViewModel?.OnValueChange(QuantityIdentifier.Quantity2);
+        }
+
+        private void Value1_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var textBox = sender as TextBox;
+                if (textBox != null)
+                {
+                    ViewModel?.PerformConversion(QuantityIdentifier.Quantity1, textBox.Text);
+                }
+            }
+        }
+
+        private void Value2_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var textBox = sender as TextBox;
+                if (textBox != null)
+                {
+                    ViewModel?.PerformConversion(QuantityIdentifier.Quantity2, textBox.Text);
+                }
+            }
+        }
+
+    }
+}
